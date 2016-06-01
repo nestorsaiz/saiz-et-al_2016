3fa3a5c0be5c2f1e598eaec8e71677fded2f09a5
nestorsaiz <nestorsaiz@icloud.com>
2016-06-01 18:50:24 -0400

    updated with new panels

--- figures_1-S1.R
+++ figures_1-S1.R
@@ -18,13 +18,14 @@ idcols <- c('EPI' = 'red', 'PRE' = 'blue', 'DP' = 'purple', 'DN' = 'gray',
             'TE' = 'green', 'ICM' = 'purple')
 
 # Figure 1c
-## Scatter plots for the levels of GATA6 and NANOG 
+## Scatter plots showing the levels of GATA6 and NANOG in log scale
 ## in individual ICM cells of embryos collected at sequential stages 
 ## of development as in Fig. 1b ('Littermates', from 32-cells onwards). 
 ## Each dot represents a single ICM cell.
 fig1c <- ggplot(FGF.all %>% 
                         ## Select ICM cells of Littermates with 32 or more cells
                         filter(Treatment == 'Littermate',
+                               Exp_date != '20150820', 
                                TE_ICM != 'TE', 
                                Stage != '<32'),
                 ## Plot corrected log of GATA6 (CH4) vs NANOG (CH5) fluorescence
@@ -44,86 +45,114 @@ fig1c <- fig1c + coord_fixed()
 ## Print the plot to the screen
 print(fig1c)
 
+## Bar plot representing the composition for each embryo in Fig. 1c (all lineages)
+## ordered by total cell number, represented as total number of cells
+# fig1d <- ggplot(FGF.all %>% 
+#                         ## Select Littermates with 32 or more cells
+#                         filter(Treatment == 'Littermate',
+#                                Stage != '<32'),
+#                 ## Plot total cell number as a factor on the X axis
+#                 ## and identity (% of ICM), assigned automatically, on the Y axis
+#                 aes(x = reorder(Embryo_ID, Cellcount), fill = Identity.auto), na.rm = TRUE)
+# fig1d <- fig1d + geom_bar()
+# ## Color-code for identity
+# fig1d <- fig1d + scale_fill_manual(values = idcols)
+# ## Set aesthetics for the plot
+# fig1d <- fig1d + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# fig1d <- fig1d + labs(fill = 'Identity', x = 'Total cell number')
+# fig1d <- fig1d + coord_fixed(0.25)
+# ## Print the plot to the screen
+# print(fig1d)
+
 # Figure 1d
-## Bar plots representing the ICM composition for each embryo in Fig. 1c
-## ordered by total cell number, represented as percentage of ICM cells
+## Bar plot representing the lineage composition for each embryo in Fig. 1c (all lineages)
+## ordered by total cell number, represented as percentage of the total
 fig1d <- ggplot(FGF.all %>% 
-                        ## Select ICM cells of Littermates with 32 or more cells
+                        ## Select Littermates with 32 or more cells
                         filter(Treatment == 'Littermate',
-                               TE_ICM != 'TE', 
+                               Exp_date != '20150820', 
                                Stage != '<32'),
-                ## Plot total cell number as a factor on the X axis
-                ## and identity (% of ICM), assigned automatically, on the Y axis
-            aes(x = factor(Cellcount), fill = Identity.auto), na.rm = TRUE)
+                ## Plot single embryos, ordered by total cell number, on the X axis
+                ## and identity (% of total), assigned automatically, on the Y axis
+                aes(x = reorder(Embryo_ID, Cellcount), fill = Identity.auto), 
+                na.rm = TRUE)
 fig1d <- fig1d + geom_bar(position = 'fill')
 ## Color-code for identity
 fig1d <- fig1d + scale_fill_manual(values = idcols)
 ## Set aesthetics for the plot
-fig1d <- fig1d + theme_bw()
-fig1d <- fig1d + labs(fill = 'Identity', x = 'Total cell number', y = '% of ICM')
-fig1d <- fig1d + coord_fixed(15)
+fig1d <- fig1d + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+fig1d <- fig1d + labs(fill = 'Identity', x = 'Total cell number', y = '% of total')
+fig1d <- fig1d + coord_fixed(25)
 ## Print the plot to the screen
 print(fig1d)
 
 # Figure 1e
-## Bar plots representing ICM composition of embryos in Fig. 1c
-## binned in five developmental stages 
+## Bar plots representing the lineage composition of embryos in Fig. 1c
+## binned into five developmental stages 
 fig1e <- ggplot(FGF.all %>% 
-                        ## Select ICM cells of Littermates with 32 or more cells
+                        ## Select Littermates with 32 or more cells
                         filter(Treatment == 'Littermate',
-                               TE_ICM != 'TE', 
+                               Exp_date != '20150820', 
                                Stage != '<32'), 
                 ## Plot stage on the X axis
-                ## and identity (% of ICM), assigned automatically, on the Y axis
+                ## and identity (% of total), assigned automatically, on the Y axis
              aes(x = Stage, fill = Identity.auto))
 fig1e <- fig1e + geom_bar(position = 'fill')
 ## Color-code for identity
 fig1e <- fig1e + scale_fill_manual(values = idcols)
 ## Set aesthetis for the plot
-fig1e <- fig1e + theme_bw()
-fig1e <- fig1e + labs(fill = 'Identity', x = 'Stage', y = '% of ICM')
+fig1e <- fig1e + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+fig1e <- fig1e + labs(fill = 'Identity', x = 'Stage', y = '% of total')
 fig1e <- fig1e + coord_fixed(10)
 ## Print the plot to the screen
 print(fig1e)
 
 # Figure 1f
-## Bar plots representing the proportion of TE vs ICM for each embryo in Fig. 1c
-## ordered by total cell number, represented as percentage of the total
+## Bar plots representing the ICM composition for each embryo in Fig. 1c
+## ordered by total cell number, represented as percentage of ICM cells
 fig1f <- ggplot(FGF.all %>% 
-                        ## Select Littermates with 32 or more cells
+                        ## Select ICM cells of Littermates with 32 or more cells
                         filter(Treatment == 'Littermate', 
+                               Exp_date != '20150820', 
+                               TE_ICM == 'ICM',
                                Stage != '<32'), 
-                ## Plot total cell number as a factor on the X axis
-                ## and % of TE vs ICM identity on the Y axis
-                aes(x = factor(Cellcount), fill = TE_ICM))
+                ## Plot single embryos, ordered by total cell number, on the X axis
+                ## and identity (% of ICM), assigned automaticallyl, on the Y axis
+                aes(x = reorder(Embryo_ID, Cellcount), fill = Identity.auto), 
+                na.rm = TRUE)
 fig1f <- fig1f + geom_bar(position = 'fill')
-## Color-code for TE vs ICM
+## Color-code for identity
 fig1f <- fig1f + scale_fill_manual(values = idcols)
 ## Set up aesthetics for the plot
-fig1f <- fig1f + theme_bw() + coord_fixed(15)
-fig1f <- fig1f + labs(fill = 'Identity', x = 'Total cell number', y = '% of total')
+fig1f <- fig1f + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+fig1f <- fig1f + labs(fill = 'Identity', x = 'Total cell number', y = '% of ICM')
+fig1f <- fig1f + coord_fixed(25)
 ## Print the plot to the screen
 print(fig1f)
 
 # Figure 1g
-## Bar plots representing the proportion of TE vs ICM of embryos in Fig. 1c
+## Bar plots representing the ICM composition of embryos in Fig. 1c
 ## binned in five developmental stages 
 fig1g <- ggplot(FGF.all %>% 
-                        ## Select Littermates with 32 or more cells
-                        filter(Treatment == 'Littermate', 
+                        ## Select ICM cells of Littermates with 32 or more cells
+                        filter(Treatment == 'Littermate',
+                               Exp_date != '20150820', 
+                               TE_ICM == 'ICM',
                                Stage != '<32'), 
                 ## Plot stage on the X axis
                 ## and % of TE vs ICM identity on the Y axis
-                aes(x = Stage, fill = TE_ICM))
+                aes(x = Stage, fill = Identity.auto))
 fig1g <- fig1g + geom_bar(position = 'fill')
 ## Color-code for TE vs ICM
 fig1g <- fig1g + scale_fill_manual(values = idcols)
 ## Set up aesthetics for the plot
-fig1g <- fig1g + theme_bw()
-fig1g <- fig1g + labs(fill = 'Identity', x = 'Stage', y = '% of total')
+fig1g <- fig1g + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+fig1g <- fig1g + labs(fill = 'Identity', x = 'Stage', y = '% of ICM')
 fig1g <- fig1g + coord_fixed(10)
 ## Print the plot to the screen
 print(fig1g)
+
+--------------------------------------------------------------------------------
 
 # Figure S1a
 ## Boxplot of total cell number for embryos in Fig. 1
@@ -136,6 +165,7 @@ figS1a <- ggplot(FGF.all %>%
                                   Stage) %>%
                          summarise() %>% 
                          filter(Treatment == 'Littermate', 
+                                Exp_date != '20150820', 
                                 Stage != '<32'), 
                  aes(x = Stage, y = Cellcount))
 ## Create boxplots for each stage
@@ -155,6 +185,7 @@ print(figS1a)
 figS1b <- ggplot(FGF.ICMsum %>%
                          ## Select Littermates with more than 32 cells
                          filter(Treatment == 'Littermate', 
+                                Exp_date != '20150820', 
                                 Stage != '<32'), 
                  ## Plot number of cells for each lineage on the Y axis
                  aes(x = Stage, y = Count))
@@ -178,6 +209,7 @@ print(figS1b)
 figS1c <- ggplot(FGF.sum %>% 
                          ## Select Littermates with more than 32 cells
                          filter(Treatment == 'Littermate', 
+                                Exp_date != '20150820', 
                                 Stage != '<32'), 
                  ## Plot number of ICM or TE cells on the Y axis
                  aes(x = Stage, y = Count))
@@ -187,3 +219,52 @@ figS1c <- figS1c + scale_fill_manual(values = idcols)
 figS1c <- figS1c + theme_bw() + coord_fixed(0.075)
 figS1c <- figS1c + labs(fill = 'Identity', x = 'Stage', y = 'Number of cells')
 print(figS1c)
+
+# Figure S1d
+## Bar plot representing the lineage composition for each embryo in Fig. 1c (all lineages)
+## ordered by total cell number, represented as cells per lineage
+figs1d <- ggplot(FGF.all %>% 
+                        ## Select Littermates with 32 or more cells
+                        filter(Treatment == 'Littermate',
+                               Exp_date != '20150820', 
+                               Stage != '<32'),
+                ## Plot single embryos, ordered by total cell number, on the X axis
+                ## and identity (% of total), assigned automatically, on the Y axis
+                aes(x = reorder(Embryo_ID, Cellcount), fill = Identity.auto), 
+                na.rm = TRUE)
+figs1d <- figs1d + geom_bar()
+## Color-code for identity
+figs1d <- figs1d + scale_fill_manual(values = idcols)
+## Set aesthetics for the plot
+figs1d <- figs1d + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+figs1d <- figs1d + labs(fill = 'Identity', x = 'Total cell number', y = 'Cell number')
+figs1d <- figs1d + coord_fixed(0.25)
+## Print the plot to the screen
+print(figs1d)
+
+# Figure S1e
+## Scatter plots showing the levels of GATA6 and NANOG in linear scale
+## in individual ICM cells of embryos collected at sequential stages 
+## of development as in Fig. 1b ('Littermates', from 32-cells onwards). 
+## Each dot represents a single ICM cell.
+figs1e <- ggplot(FGF.all %>% 
+                        ## Select ICM cells of Littermates with 32 or more cells
+                        filter(Treatment == 'Littermate',
+                               Exp_date != '20150820', 
+                               TE_ICM != 'TE', 
+                               Stage != '<32'),
+                ## Plot corrected log of GATA6 (CH4) vs NANOG (CH5) fluorescence
+                aes(x = exp(CH4.ebLogCor), y = exp(CH5.ebLogCor)))
+## Color-code for identity, as assigned automatically with 'identify.R'
+figs1e <- figs1e + geom_point(aes(color = Identity.auto), size = I(1.2))
+## Overlay 
+figs1e <- figs1e + scale_color_manual(values = idcols)
+## Set limits for X and Y axes
+figs1e <- figs1e + xlim(0, 255) + ylim(0, 255)
+## Set aesthetis for the plot
+figs1e <- figs1e + theme_bw()
+figs1e <- figs1e + labs(color = 'Identity', x = '[GATA6]', y = '[NANOG]')
+figs1e <- figs1e + facet_grid( ~ Stage)
+figs1e <- figs1e + coord_fixed()
+## Print the plot to the screen
+print(figs1e)
